6e2b3116d9dea941e9e9756973f94baec24c1aa0
mapandubot <mapanduserbot@gmail.com>
2025-02-27 12:26:43 +0700

    Update misc.py

--- AyiinXd/ayiin/misc.py
+++ AyiinXd/ayiin/misc.py
@@ -198,7 +198,7 @@ async def create_quotly(
 async def Carbon(
     code,
     base_url="https://carbonara-42.herokuapp.com/api/cook",
-    file_name="Kazu-Userbot",
+    file_name="Mapand-Userbot",
     **kwargs,
 ):
     kwargs["code"] = code
