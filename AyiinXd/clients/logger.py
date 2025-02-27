9f1ca03b4f80a4ee5a9fae1555abf5813a98e0f9
mapandubot <mapanduserbot@gmail.com>
2025-02-27 12:00:13 +0700

    Update logger.py

--- AyiinXd/clients/logger.py
+++ AyiinXd/clients/logger.py
@@ -18,11 +18,11 @@ from AyiinXd.ayiin import ayiin_version as py_ver
 from AyiinXd.ayiin import HOSTED_ON, checking
 
 MSG_ON = """
-❏ 𝙺𝙰𝚉𝚄 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪғᴋᴀɴ
+❏ ᴍᴀᴘᴀɴᴅ ᴜsᴇʀʙᴏᴛ ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪғᴋᴀɴ
 ╭╼┅━━━━━╍━━━━━┅╾
-├▹ 𝙺𝙰𝚉𝚄 Vᴇʀsɪᴏɴ - {} •[{}]•
+├▹ ᴍᴀᴘᴀɴᴅ ᴜsᴇʀʙᴏᴛ Vᴇʀsɪᴏɴ - {} •[{}]•
 ├▹ 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 Vᴇʀsɪᴏɴ - {}
-├▹ @disinikazu-Userbot
+├▹ Mapand-Userbot
 ├▹ Kᴇᴛɪᴋ .alive Uɴᴛᴜᴋ Mᴇɴɢᴇᴄᴇᴋ Bᴏᴛ
 ╰╼┅━━━━━╍━━━━━┅╾
 """
@@ -50,7 +50,7 @@ async def ayiin_userbot_on():
         if bot and tgbot:
             AyiinUBOT = await tgbot.get_me()
             BOT_USERNAME = AyiinUBOT.username
-            await bot(EditAdminRequest(BOTLOG_CHATID, BOT_USERNAME, new_rights, "𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃 𝙺𝙰𝚉𝚄"))
+            await bot(EditAdminRequest(BOTLOG_CHATID, BOT_USERNAME, new_rights, "𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃 ᴍᴀᴘᴀɴᴅ ᴜsᴇʀʙᴏᴛ"))
             await asyncio.sleep(3)
     except BaseException:
         pass
