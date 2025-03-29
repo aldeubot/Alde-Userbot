1b76008edd7a8418c579e639c042e3c4f3973537
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-03-29 21:29:19 +0700

    Update tools.py

--- AyiinXd/ayiin/tools.py
+++ AyiinXd/ayiin/tools.py
@@ -339,8 +339,8 @@ async def bash(cmd):
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Kazu-Userbot"
-    auth_url = "https://github.com/ionmusic/Kazu-Userbot"
+    auth_name = "Alde-Userbot"
+    auth_url = "https://github.com/aldeubot/Alde-Userbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,
