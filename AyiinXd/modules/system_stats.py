48ee1a63d3c822b700799a74ab80e16d86dd8b0c
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-03-29 23:58:25 +0700

    Update system_stats.py

--- AyiinXd/modules/system_stats.py
+++ AyiinXd/modules/system_stats.py
@@ -109,8 +109,8 @@ async def psu(event):
     help_string += "**Iɴғᴏʀᴍᴀsɪ Mᴇsɪɴ**\n"
     help_string += f"**Pʏᴛʜᴏɴ :** `{sys.version}`\n"
     help_string += f"**Tᴇʟᴇᴛʜᴏɴ :**`{__version__}`\n"
-    help_string += f"**Pʏ-ᴋᴀᴢᴜ :** `{py_ver}`\n"
-    help_string += f"**ᴋᴀᴢᴜ ᴠᴇʀsɪᴏɴ :** `{ayiin_version} [{HOSTED_ON}]`"
+    help_string += f"**Pʏ-ᴀʟᴅᴇ :** `{py_ver}`\n"
+    help_string += f"**ᴀʟᴅᴇ ᴠᴇʀsɪᴏɴ :** `{ayiin_version} [{HOSTED_ON}]`"
     await edit_or_reply(event, help_string)
 
 
@@ -187,7 +187,7 @@ async def amireallyalive(alive):
     await alive.edit("👻")
     await asyncio.sleep(3)
     output = (
-        f"**Tʜᴇ [ᴋᴀᴢᴜ ᴜsᴇʀʙᴏᴛ](https://github.com/ionmusic/Kazu-Userbot)**\n\n"
+        f"**Tʜᴇ [ᴀʟᴅᴇ ᴜsᴇʀʙᴏᴛ](https://github.com/aldeubot/Alde-Userbot)**\n\n"
         f"**{alive_text}**\n\n"
         f"╭✠╼━━━━━━━━━━━━━━━✠╮\n"
         f"{emoji} **Bᴀsᴇ Oɴ :** ••{adB.name}••\n"
@@ -197,8 +197,8 @@ async def amireallyalive(alive):
         f"{emoji} **Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{python_version()}` \n"
         f"{emoji} **PʏTɢCᴀʟʟs Vᴇʀsɪᴏɴ :** `{pytgcallss}` \n"
         f"{emoji} **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{version.__version__}` \n"
-        f"{emoji} **Pʏ-ᴋᴀᴢᴜ ᴠᴇʀsɪᴏɴ :** `{py_ver}`\n"
-        f"{emoji} **ᴋᴀᴢᴜ ᴠᴇʀsɪᴏɴ :** `{ayiin_version}` [{HOSTED_ON}]\n"
+        f"{emoji} **Pʏ-ᴀʟᴅᴇ ᴠᴇʀsɪᴏɴ :** `{py_ver}`\n"
+        f"{emoji} **ᴀʟᴅᴇ ᴠᴇʀsɪᴏɴ :** `{ayiin_version}` [{HOSTED_ON}]\n"
         f"{emoji} **Bᴏᴛ Uᴘᴛɪᴍᴇ :** `{uptime}`\n"
         "╰✠╼━━━━━━━━━━━━━━━✠╯\n\n"
     )
