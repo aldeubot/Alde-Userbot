df550e52c71d73e4334d3460fe8bf51ac6f0b6d1
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-03-30 00:36:20 +0700

    Update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -39,7 +39,7 @@ try:
     LOGS.info(f"PyTgCalls Version - {pytgcalls}")
     LOGS.info(f"Userbot Version - {ubotversion} •[{adB.name}]•")
     LOGS.info(f"Alde Version - {ayiin_version} •[{HOSTED_ON}]•")
-    LOGS.info("[✨ALDE USERBOT BERHASIL DIAKTIFKAN! ✨]")
+    LOGS.info("[ᯓ★ ALDE USERBOT BERHASIL DIAKTIFKAN! ᯓ★]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
     pass
 except BaseException as e:
