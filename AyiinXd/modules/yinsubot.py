8b5fa9c5aa22e6d4563ba03c9a2159597b90f870
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-04-02 18:53:10 +0700

    Update yinsubot.py

--- AyiinXd/modules/yinsubot.py
+++ AyiinXd/modules/yinsubot.py
@@ -142,7 +142,7 @@ async def _(d):
 
 CMD_HELP.update(
     {
-        "yinsubot": f"**Plugin : **`Kazu-Userbot`\
+        "yinsubot": f"**Plugin : **`Alde-Userbot`\
         \n\n  »  **Perintah :** `{cmd}sadboy`\
         \n  »  **Kegunaan : **Gombalan sad\
         \n\n  »  **Perintah :** `{cmd}wah`\
