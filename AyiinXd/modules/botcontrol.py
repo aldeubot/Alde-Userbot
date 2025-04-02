fcfb240cc3e57d7a0b506325eee5d972a1009c04
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-04-02 13:57:29 +0700

    Update botcontrol.py

--- AyiinXd/modules/botcontrol.py
+++ AyiinXd/modules/botcontrol.py
@@ -1042,7 +1042,7 @@ async def bot_start(event):
                         \n\n**Saya adalah {my_first}** \
                         \n**Anda dapat menghubungi [{OWNER}](tg://user?id={OWNER_ID}) dari sini.**\
                         \n**Jangan melakukan spam atau anda akan di Banned**\
-                        \n\n**Powered by 🐣** [𝙺𝙰𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://github.com/ionmusic/Kazu-Userbot)"
+                        \n\n**Powered by 🐣** [ᴀʟᴅᴇ-ᴜꜱᴇʀʙᴏᴛ](https://github.com/aldeubo/Alde-Userbot)"
         buttons = [
             (
                 Button.url("ɢʀᴏᴜᴘ", f"https://t.me/{GROUP}"),
