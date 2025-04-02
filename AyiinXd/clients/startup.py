a11a1cb26d2e9b25f72943ef5c7508429ab804cb
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-04-02 19:42:01 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -23,7 +23,7 @@ from AyiinXd import (
 )
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nAlde-UserBot v{}, Copyright © 2021-2025 Aldean• <https://github.com/aldeubot>"
+EOL = "EOL\nKazu-UserBot v{}, Copyright © 2021-2022 Kazu• <https://github.com/ionmusic>"
 MSG_BLACKLIST = "\nAlde-UserBot v{}, Copyright © 2021-2025 Aldean• <https://github.com/aldeubot>"
 
 
