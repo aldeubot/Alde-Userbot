8e40b68e7c18b012aa9b3ef4295e07bc87038589
ᴀʟᴅᴇᴀɴ <aldeuserbot@gmail.com>
2025-04-04 00:24:06 +0700

    Update toolsyins.py

--- AyiinXd/ayiin/toolsyins.py
+++ AyiinXd/ayiin/toolsyins.py
@@ -15,6 +15,7 @@ from telethon.tl.types import MessageService
 
 
 async def eor(event, text=None, **args):
+    ok = None
     time = args.get("time", None)
     edit_time = args.get("edit_time", None)
     if "edit_time" in args:
@@ -24,6 +25,7 @@ async def eor(event, text=None, **args):
     if "link_preview" not in args:
         args["link_preview"] = False
     args["reply_to"] = event.reply_to_msg_id or event
+
     if event.out and not isinstance(event, MessageService):
         if edit_time:
             await sleep(edit_time)
@@ -45,12 +47,11 @@ async def eor(event, text=None, **args):
     else:
         ok = await event.client.send_message(event.chat_id, text, **args)
 
-    if time:
+    if time and ok:
         await sleep(time)
         return await ok.delete()
         
     return ok
-
 async def eod(event, text=None, **kwargs):
     kwargs["time"] = kwargs.get("time", 8)
     return await eor(event, text, **kwargs)
